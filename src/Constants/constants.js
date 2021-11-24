95a017f6fb1952952c095cf7025f53824c44f225
ayushchauhan248 <ayush.chauhan_cs19@gla.ac.in>
2021-11-25 02:32:41 +0530

    Changed db_url

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,2 +1,2 @@
-module.exports.db_url = "mongodb+srv://ayush:rahaish3@rahaish.hplmj.mongodb.net/Rahaish?retryWrites=true&w=majority";
-// module.exports.db_url = "mongodb://localhost:27017/rahaish";
+// module.exports.db_url = "mongodb+srv://ayush:rahaish3@rahaish.hplmj.mongodb.net/Rahaish?retryWrites=true&w=majority";
+module.exports.db_url = "mongodb://localhost:27017/rahaish";
